1692ca6564488626f084d1ca97f31392df41adcc
devH <146154052+0umj@users.noreply.github.com>
2024-11-14 10:38:47 +0900

    feat: 클릭 파티클 시스템 기능 추가

---
+++ Assets/@Scripts/System/ClickParticleSystem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickParticleSystem
+{
+
+    private ParticleSystem particleSystem;
+
+    public ClickParticleSystem(Button button)
+    {
+        particleSystem = button.GetComponentInChildren<ParticleSystem>();
+        button.onClick.AddListener(StartParticleSystem);
+    }
+
+    public void StartParticleSystem()
+    {
+        particleSystem.Play();
+    }
+
+}
